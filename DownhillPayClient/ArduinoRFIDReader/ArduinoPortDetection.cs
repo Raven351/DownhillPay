d6a8e8e6c42269e9ff11bb749c67e1c5f01f5d77
Bartosz Baum <bart.baum1@gmail.com>
2019-04-29 20:08:20 +0200

    Add library ArduinoRFIDReader and class ArduinoPortDetection.

---
+++ DownhillPayClient/ArduinoRFIDReader/ArduinoPortDetection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoRFIDReader
+{
+    /// <summary>
+    /// Includes methods for Arduino device detection. 
+    /// </summary>
+    public class ArduinoPortDetection
+    {
+        /// <summary>
+        /// Returns port name from Win32_SerialPort with 'Arduino' phrase in it's description.
+        /// </summary>
+        /// <returns>Arduino device port name.</returns>
+        public static string AutodetectPort()
+        {
+            ManagementScope connectionScope = new ManagementScope();
+            SelectQuery serialQuery = new SelectQuery("SELECT * FROM Win32_SerialPort");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, serialQuery);
+
+            try
+            {
+                foreach (ManagementObject item in searcher.Get())
+                {
+                    string desc = item["Description"].ToString();
+                    string deviceId = item["DeviceID"].ToString();
+
+                    if (desc.Contains("Arduino"))
+                    {
+                        return deviceId;
+                    }
+                }
+            }
+            catch (ManagementException e)
+            {
+                /* Do Nothing */
+            }
+
+            return null;
+        }
+    }
+}
