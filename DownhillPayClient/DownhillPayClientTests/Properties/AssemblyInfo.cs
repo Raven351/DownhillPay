97e45da11b531c6a9d0c9c433d59be458db1269a
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2020-01-11 21:09:28 +0100

    Add Tests to project. Add tests for class NewPersonalizedCardFormUserControl.

---
+++ DownhillPayClient/DownhillPayClientTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DownhillPayClientTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DownhillPayClientTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("348c4bc5-2b36-4a6e-98c4-ee42074092e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DownhillPayClient/DownhillPayClientTests/UserControlsTests/NewPersonalizedCardUCTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DownhillPayClient;
+using DownhillPayClient.UserControls;
+using System.Windows.Controls;
+
+namespace DownhillPayClientTests
+{
+    [TestClass]
+    public class NewPersonalizedCardUCTests
+    {
+        readonly PrivateObject NewPersonalizedCardUC = new PrivateObject(new NewPersonalizedCardFormUserControl());
+
+        private void SetTextboxText(string fieldToSet, string textToSet)
+        {
+            TextBox textBox = new TextBox
+            {
+                Text = textToSet
+            };
+            NewPersonalizedCardUC.SetField(fieldToSet, textBox);
+        }
+
+        private void IsFirstNameCorrectTest(string textboxText, bool assertValue)
+        {
+            SetTextboxText("firstNameTextBox", textboxText);
+            Assert.AreEqual(assertValue, NewPersonalizedCardUC.Invoke("IsFirstNameCorrect"));
+        }
+
+        [TestMethod]
+        public void IsFirstNameCorrectTestEmpty()
+        {
+            IsFirstNameCorrectTest("", false);
+        }
+
+        [TestMethod]
+        public void IsFirstNameCorrectTestOneChar()
+        {
+            IsFirstNameCorrectTest("A", false);
+        }
+
+        [TestMethod]
+        public void IsFirstNameCorrectTestTwoChars()
+        {
+            IsFirstNameCorrectTest("As", true);
+        }
+    }
+}
